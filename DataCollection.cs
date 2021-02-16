61a5d15ca3b5657c1c49974ff11c9bb8389967fd
takatlssx <69699607+takatlssx@users.noreply.github.com>
2021-02-16 19:11:53 +0900

    Update DataCollection.cs

--- DataCollection.cs
+++ DataCollection.cs
@@ -1,160 +1,168 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
 
-namespace Test
+namespace MovieDataBase
 {
     public class DataCollection
     {
         public string Error;
         public string Msg;
-    
+
         public string Name;
         public string RootDir;
         public string InfoPath;
-        
+
         public List<string> TableNames;
         public List<string> RelationalTableNames;
         public List<string> SubTableNames;
         public string MainTableName;
-        
-        public Dictionary<string,DataCollectionTable> Tables = new Dictionary<string,DataCollectionTable>(); 
+
+        public Dictionary<string, DataCollectionTable> Tables = new Dictionary<string, DataCollectionTable>();
         public DataCollectionTable MainTable;
-        
-        public DataCollection(string name,string rootDir)
+
+        public DataCollection(string name, string rootDir)
         {
             Name = name;
             RootDir = rootDir;
             InfoPath = rootDir + "\\" + name + ".info";
-            
-            if(GetInfo())
+
+            if (GetInfo())
             {
-                GetData();
+                GetTableData();
             }
         }
-        
+
         public bool GetInfo()
         {
             Error = $"設定取得エラー:DataCollection.GetInfo()\r\n";
-            
-            if(!File.Exists(InfoPath))
+
+            if (!File.Exists(InfoPath))
             {
                 Error += $"infoファイル:{InfoPath}が見つかりませんでした。\r\n";
-                retrun false;
+                return false;
             }
-            
+
             try
             {
-                using(StreamReader sr = new StreamReader(InfoPath))
+                using (StreamReader sr = new StreamReader(InfoPath))
                 {
                     string line;
                     string processingTableName = "";
-                    while((line = sr.ReadLine()) != null)
+                    while ((line = sr.ReadLine()) != null)
                     {
                         var contents = line.Split(':');
-                        
-                        if(contents[0] == "tableNames")
+
+                        if (contents[0] == "tableNames")
                         {
                             TableNames = contents[1].Split(',').ToList();
                         }
-                        else if(contents[0] == "relationalTableNames")
+                        else if (contents[0] == "relationalTableNames")
                         {
                             RelationalTableNames = contents[1].Split(',').ToList();
                         }
-                        else if(contents[0] == "subTableNames")
+                        else if (contents[0] == "subTableNames")
                         {
                             SubTableNames = contents[1].Split(',').ToList();
                         }
-                        else if(contents[0] == "mainTableName")
+                        else if (contents[0] == "mainTableName")
                         {
                             MainTableName = contents[1];
                         }
-                        else if(contents[0] == "@tableInfo")
+                        else if (contents[0] == "@tableInfo")
                         {
                             processingTableName = "";
                         }
-                        else if(contents[0] == "tableName")
+                        else if (contents[0] == "tableName")
                         {
                             processingTableName = contents[1];
-                            Tables[processingTableName] = new DataCollectionTable(processingTableName,RootDir+"\\"+processingTableName+".dc");
-                            if(processingTableName == MainTableNames)
+                            Tables[processingTableName] = new DataCollectionTable(processingTableName, RootDir + "\\" + processingTableName + ".dc");
+                            if (processingTableName == MainTableName)
                             {
                                 MainTable = Tables[processingTableName];
                             }
                         }
-                        else if(contents[0] == "index")
+                        else if (contents[0] == "index")
                         {
                             Tables[processingTableName].Index = contents[1].Split(',').ToList();
                         }
-                        else if(contents[0] == "type")
+                        else if (contents[0] == "type")
                         {
                             var buff = contents[1].Split(',');
-                            if(Tables[processingTableName].Index.Count != buff.Length)
+                            if (Tables[processingTableName].Index.Count != buff.Length)
                             {
                                 Error += $"{processingTableName}テーブルのtypeリストの要素数:{buff.Length}は不正な値です。\r\n";
                                 return false;
                             }
-                            
-                            for(int i = 0; i < Tables[processingTableName].Index.Count ; i++)
+
+                            for (int i = 0; i < Tables[processingTableName].Index.Count; i++)
                             {
                                 Tables[processingTableName].Type[Tables[processingTableName].Index[i]] = buff[i];
                             }
                         }
-                        else if(contents[0] == "empty")
+                        else if (contents[0] == "empty")
                         {
                             var buff = contents[1].Split(',');
-                            if(Tables[processingTableName].Index.Count != buff.Length)
+                            if (Tables[processingTableName].Index.Count != buff.Length)
                             {
                                 Error += $"{processingTableName}テーブルのemptyリストの要素数:{buff.Length}は不正な値です。\r\n";
                                 return false;
                             }
-                            
-                            for(int i = 0; i < Tables[processingTableName].Index.Count ; i++)
+
+                            for (int i = 0; i < Tables[processingTableName].Index.Count; i++)
                             {
                                 Tables[processingTableName].Empty[Tables[processingTableName].Index[i]] = buff[i];
                             }
                         }
-                        else if(contents[0] == "alias")
+                        else if (contents[0] == "alias")
                         {
                             var buff = contents[1].Split(',');
-                            if(Tables[processingTableName].Index.Count != buff.Length)
+                            if (Tables[processingTableName].Index.Count != buff.Length)
                             {
                                 Error += $"{processingTableName}テーブルのaliasリストの要素数:{buff.Length}は不正な値です。\r\n";
                                 return false;
                             }
-                            
-                            for(int i = 0; i < Tables[processingTableName].Index.Count ; i++)
+
+                            for (int i = 0; i < Tables[processingTableName].Index.Count; i++)
                             {
                                 Tables[processingTableName].Alias[Tables[processingTableName].Index[i]] = buff[i];
                             }
                         }
-                        else if(contents[0] == "primaryKey")
+                        else if (contents[0] == "primaryKey")
                         {
-                            if(contents[1] == null || contents[1] == "" || !Tables[processingTableName].Index.Contains(contents[1]))
+                            if (contents[1] == null || contents[1] == "" || !Tables[processingTableName].Index.Contains(contents[1]))
                             {
                                 Error += $"{processingTableName}テーブルのprimaryKey:{contents[1]}は不正な値です。\r\n";
                                 return false;
                             }
-                            Tables[processingTableName].Primarykey = contents[1]; 
+                            Tables[processingTableName].PrimaryKey = contents[1];
                         }
                     }
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                Error += $"infoファイル読み込みに失敗しました。\r\n{ex.ToString()}\r\n}";
-                retrun false;
+                Error += $"infoファイル読み込みに失敗しました。\r\n{ex.ToString()}\r\n";
+                return false;
             }
+            return true;
         }
-        
+
         public bool GetTableData()
         {
             Error = $"データ取得エラー:DataCollection.GetData()\r\n";
-            foreach(string tblName in TableNames)
+            foreach (string tblName in TableNames)
             {
-                if(!Tables[tblName].GetData){
+                if (!Tables[tblName].GetData())
+                {
                     Error += Tables[tblName].Error;
                     return false;
                 }
             }
+            return true;
         }
-    }    
+    }
 }
