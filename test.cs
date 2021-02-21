cbb405b6089622eabc57375cbd0a830f9cbe1cf3
takatlssx <69699607+takatlssx@users.noreply.github.com>
2021-02-21 12:51:10 +0900

    Update test.cs

--- test.cs
+++ test.cs
@@ -9,6 +9,21 @@ namespace MovieDataBase
 {
     public class DataCollection
     {
+        
+        public bool BackUp()
+        {
+            //コピー元のファイル一覧取得 
+            
+            //コピー先のファイル一覧取得
+            
+            //コピー先一覧とコピー元一覧のリストを比較
+            //１：コピー先に無いファイル（新規かリネームされたか）
+            //２：ファイル名は同じでも更新日時・作成日時が新しい場合
+            //３：ファイル名が同じでもファイルデータサイズが違う場合
+            //以上をcopyFileListとしてリスト化
+        }
+        
+        
         private bool rollBack()
         {
             string error = "ロールバックエラー:DataCollection.rollBack()\r\n";
