cdb3911cf8dd9eebd1f8a59178f5b59bff1f4940
takatlssx <69699607+takatlssx@users.noreply.github.com>
2021-02-21 12:13:46 +0900

    Update test.cs

--- test.cs
+++ test.cs
@@ -1,1 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
 
+namespace MovieDataBase
+{
+    public class DataCollection
+    {
+        private bool rollBack()
+        {
+            string error = "ロールバックエラー:DataCollection.rollBack()\r\n";
+            
+            string backupDir = RootDir + "\\backup";
+            
+            //バックアップフォルダの存在チェック
+            if(!Directory.Exists(backupDir))
+            {
+                Error = error + $"バックアップフォルダ:{backupDir}が見つかりませんでした。\r\n";
+                return false;
+            }
+            
+            //バックアップフォルダ内の.dc.backupファイルの一覧を取得
+            
+            //.dc.backupファイルをルートフォルダに.backupを削除しコピー。
+            
+            
+            return true;
+        }
+        
+        private bool backup()
+        {
+            string error = "バックアップエラー:DataCollection.backup()\r\n";
+            
+            string backupDir = RootDir + "\\backup";
+            
+            //バックアップフォルダの存在チェック
+            if(!Directory.Exists(backupDir))
+            {
+                Error = error + $"バックアップフォルダ:{backupDir}が見つかりませんでした。\r\n";
+                return false;
+            }
+            
+            //ルートフォルダ内の.dcファイル一覧を取得
+            
+            //.dcファイルをバックアップフォルダに「〇〇.dc.backup」という名前で保存
+            
+            return true;
+        }
+    }
+}
