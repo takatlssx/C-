f6a9912a3b5cafe16393845f49d3ff8b1fe1408d
takatlssx <69699607+takatlssx@users.noreply.github.com>
2021-02-28 12:13:57 +0900

    Update test.cs

--- test.cs
+++ test.cs
@@ -9,6 +9,132 @@ namespace MovieDataBase
 {
     public class DataCollection
     {
+        public List<Dictionari<string,string>> ViewingData;
+        public string ViewingStatusText = "";
+        public string SystemStatusText = "";
+        
+        public List<string> searchIdList = new List<string>();
+        public List<string> searchWordList = new List<string>();
+        public List<string> searchOperandList = new List<string>();
+        public string searchCombination = "or";
+        
+        public void SetData(string dataFlg = "all",string sortOrder = "asc")
+        {
+            SystemStatusText = "データベース表示";
+            if(dataFlg == "all")
+            {
+                ViewingData = DC.MainTable.Data;
+                ViewingStatusText = $"全件表示【{ViewingData.Count}件】";
+            }
+            else //"search"
+            {
+                var res = DC.MainTable.Search(serchIdList,searchWordList,searchOperandList,searchCombination);
+                
+                ViewingStatusText = $"【検索結果】";
+                for(int i = 0 ; i < searchIdList.Count ; i++)
+                {
+                    ViewingStatusText += $"条件{i} : 列'{DC.Alias[searchIdList[i]]}' {searchOperandList[i]} '{searchWordList[i]}' "
+                }
+                if(res == null || res.Count == 0)
+                {
+                    ViewingData = null;
+                    ViewingStatusText += $"【0件】";
+                }
+                else
+                {
+                    ViewingData = res;
+                    ViewingStatusText += $"【{res.Count}件】";
+                }
+            }
+            
+            //dGV描画
+            dGV.Columns.Clear();
+            dGV.Rows.Clear();
+            
+            
+            //listBox描画
+            listCategory.Items.Clear();
+            listCategory.Items.Add("全て");
+            foreach(var dt in DC.Tables["category"].Data)
+            {
+                listCategory.Items.Add(dt["category"]);
+            }
+            listTag.Items.Clear();
+            listTag.Items.Add("全て");
+            listSeries.Items.Clear();
+            listSeries.Items.Add("全て");
+            
+            if(ViewingData == null || dataFlg == "all")
+            {
+                foreach(var dt in DC.Tables["tag"].Data)
+                {
+                    listTag.Items.Add(dt["tag"]);
+                }
+                foreach(var dt in DC.Tables["series"].Data)
+                {
+                    listSeries.Items.Add(dt["series"]);
+                }
+                
+            }
+            else
+            {
+                foreach(var dt in ViewingData)
+                {
+                    string [] buff = dt["tag"].Split('/');
+                    int listTagSelectedIndex = 0;
+                    int addCnt = 0;
+                    foreach(string bf in buff)
+                    {
+                        if(bf != "" && !listTag.Items.Contains(bf))
+                        {
+                            listTag.Items.Add(bf);
+                            addCnt++;
+                            
+                            if(searchIdList.Contains("tag") && wordList[searchIdList.IndexOf("tag")] == bf)
+                            {
+                                listTagSelectedIndex = addCnt;
+                            }
+                        }
+                    }
+                    
+                    buff = dt["series"].Split('/');
+                    int listSeriesSelectedIndex = 0;
+                    int addCntSeries = 0;
+                    foreach(string bf in buff)
+                    {
+                        if(bf != "" && !listSeries.Items.Contains(bf))
+                        {
+                            listSeries.Items.Add(bf);
+                            addCntSeries++;
+                            
+                            if(searchIdList.Contains("series") && wordList[searchIdList.IndexOf("series")] == bf)
+                            {
+                                listSeriesSelectedIndex = addCnt;
+                            }
+                        }
+                    }                    
+                }
+                listTag.SetSelect(listTagSelectedIndex,true);
+                listSeries.SetSelect(listSeriesSelectedIndex,true);
+            }
+            
+        }
+        
+        public void SetSelectedData(){
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         public List<string> SearchUnRegisteredData()
         {
             //データベースに登録されていないファイルパスを格納するリスト
