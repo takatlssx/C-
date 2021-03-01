d3d2875dbc8a42d7f2a0542698aee5397024dc52
takatlssx <69699607+takatlssx@users.noreply.github.com>
2021-03-01 12:59:23 +0900

    Update BackupTest.cs

--- BackupTest.cs
+++ BackupTest.cs
@@ -7,12 +7,26 @@ namespace XX
         public string Error = "";
         public string Msg = "";
         
+        private string srcRootDir;
+        private string destRootDir;
+        
         public Backup(string srcRootDir,string destRootDir)
         {
         }
         
         public bool BackupAll()
         {
+            //パスチェック
+            if(!Directory.Exists(srcRootDir))
+            {
+                return false;
+            }
+            
+            //ソース元フォルダのファイル一覧取得（FileInfo）
+            IEnumerable<FileInfo> srcFileList = new DirectoryInfo(srcRootDir).EnumerateFiles("*", SearchOption.AllDirectories);
+            int srcFilesSize = secFileList.Sum(x => x.Length);
+            
+            return true;
         }
     }
 }
