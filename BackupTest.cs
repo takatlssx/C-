70f5ddf4372a9021565fac665a20140f61b6f046
takatlssx <69699607+takatlssx@users.noreply.github.com>
2021-03-01 12:47:31 +0900

    Create BackupTest.cs

---
+++ BackupTest.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace XX
+{
+    public class Backup
+    {
+        public string Error = "";
+        public string Msg = "";
+        
+        public Backup(string srcRootDir,string destRootDir)
+        {
+        }
+        
+        public bool BackupAll()
+        {
+        }
+    }
+}
