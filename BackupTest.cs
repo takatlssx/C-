343a933235ad81788cb36422fb001786c08700d5
takatlssx <69699607+takatlssx@users.noreply.github.com>
2021-03-02 14:53:18 +0900

    Update BackupTest.cs

--- BackupTest.cs
+++ BackupTest.cs
@@ -21,52 +21,85 @@ namespace XX
             this.destRootDir = destRootDir;
         }
         
+        public void SetTextInvoker(string ctlName,string text)
+        {
+            ownerForm.Invoke(new Action(() => ownerForm.Controls[ctlName].Text = text));
+        }
+        
+        public void SetProgressInvoker(string ctlName,int val)
+        {
+            ownerForm.Invoke(new Action(() => ownerForm.Controls[ctlName].value = val));
+        }
+        
         public bool BackupSync()
         {
             string error = $"バックアップエラー:Backup.BackupSync()\r\n";
-            
-            //バックアップ元・バックアップ先ルートフォルダパスチェック
-            
-            //バックアップ元のファイルインフォ一覧
-            var srcFileList = new DirectoryInfo(srcRootDir).EnumerateFiles("*", SearchOption.AllDirectories).ToList();
-            //バックアップ先に作るフォルダ名一覧
-            var destDirList = Directory.EnumerateDirectories(srcRootDir, "*", SearchOption.AllDirectories).Select(x => x.Replace(srcRootDir,destRootDir)).ToList();
-            
-            //バックアップ先のファイルインフォ一覧
-            var destFileList = new DirectoryInfo(destRootDir).EnumerateFiles("*", SearchOption.AllDirectories).ToList();
-            
             //バックアップするファイルのリスト
-            List<FileInfo> newFileList = new List<FileInfo>();
-            
+            List<FileInfo> newFileList = new List<FileInfo>();            
             //バックアップ先にあってバックアップ元に無い・
             //バックアップ元と更新日時が異なるファイル名（旧ファイル）リスト
             List<string> oldFileList = new List<string>();
+            //バックアップ元のファイルインフォ一覧
+            List<FileInfo> srcFileList;
+            //バックアップ先に作るフォルダ名一覧
+            List<string> destDirList;            
+            //バックアップ先のファイルインフォ一覧
+            List<FileInfo> destFileList;
             
-            //バックアップ元にあってバックアップ先に無いファイル（新規ファイル)
-            //バックアップ元・バックアップ先の同名ファイルで更新日時が違うファイル（更新ファイル）
-            //以上をnewFileListに追加
-            foreach(FileInfo fI in srcFileList)
+            //バックアップ元・バックアップ先ルートフォルダパスチェック
+            
+            
+            SetTextInvoker("SystemStatusLabel","バックアップ情報を取得しています。。。。");
+            try
             {
-                //新規ファイル
-                if(!File.Exists(fI.FullName.Replace(srcRootDir,destRootDir)))
-                {
-                    newFileList.Add(fI);
-                }
-                else
-                {
-                    if(fI.LastWriteTime != File.GetLastWriteTime(fI.FullName.Replace(srcRootDir,destRootDir)))
-                    {
-                        newFileList.Add(fI);
-                        //更新日時が違うバックアップ先ファイルをoldFileListへ追加
-                        oldFileList.Add(fI.FullName.Replace(srcRootDir,destRootDir));
-                    }
-                }
+                //バックアップ元のファイルインフォ一覧
+                srcFileList = new DirectoryInfo(srcRootDir).EnumerateFiles("*", SearchOption.AllDirectories).ToList();
+                //バックアップ先に作るフォルダ名一覧
+                destDirList = Directory.EnumerateDirectories(srcRootDir, "*", SearchOption.AllDirectories).Select(x => x.Replace(srcRootDir,destRootDir)).ToList();            
+                //バックアップ先のファイルインフォ一覧
+                destFileList = new DirectoryInfo(destRootDir).EnumerateFiles("*", SearchOption.AllDirectories).ToList();
+            }
+            catch(Exception ex)
+            {
+                Error = error + $"バックアップ情報の取得に失敗しました。\r\n{ex.ToString()}\r\n";
+                return false;
             }
             
-            //バックアップ先にあってバックアップ元に無いファイルをoldFileListに追加
-            //但しバックアップ先のoldfileフォルダにあるファイルは除外
-            oldFileList.AddRange(destFileList.Where(x => !File.Exists(x.FullName.replace(destRootDir,srcRootDir)) && !x.FullName.Contains(destRootDir+"\\oldfile")).ToList());
+            SetTextInvoker("SystemStatusLabel","ファイルの差分を検証しています。。。。");
+            try
+            {
+                //バックアップ元にあってバックアップ先に無いファイル（新規ファイル)
+                //バックアップ元・バックアップ先の同名ファイルで更新日時が違うファイル（更新ファイル）
+                //以上をnewFileListに追加
+                foreach(FileInfo fI in srcFileList)
+                {
+                    //新規ファイル
+                    if(!File.Exists(fI.FullName.Replace(srcRootDir,destRootDir)))
+                    {
+                        newFileList.Add(fI);
+                    }
+                    else
+                    {
+                        if(fI.LastWriteTime != File.GetLastWriteTime(fI.FullName.Replace(srcRootDir,destRootDir)))
+                        {
+                            newFileList.Add(fI);
+                            //更新日時が違うバックアップ先ファイルをoldFileListへ追加
+                            oldFileList.Add(fI.FullName.Replace(srcRootDir,destRootDir));
+                        }
+                    }
+                }
+                
+                //バックアップ先にあってバックアップ元に無いファイルをoldFileListに追加
+                //但しバックアップ先のoldfileフォルダにあるファイルは除外
+                oldFileList.AddRange(destFileList.Where(x => !File.Exists(x.FullName.replace(destRootDir,srcRootDir)) && !x.FullName.Contains(destRootDir+"\\oldfile")).ToList());
+            }
+            catch(Exception ex)
+            {
+                Error = error + $"ファイルの差分情報検証に失敗しました。\r\n{ex.ToString()}\r\n";
+                return false;
+            }          
             
+            SetTextInvoker("SystemStatusLabel","フォルダ構造をコピーしています。。。。");
             //フォルダのコピー
             for(){
             }
