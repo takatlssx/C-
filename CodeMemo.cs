ffd477c50b8487d6d06fbc2c76a2a536cf168b74
takatlssx <69699607+takatlssx@users.noreply.github.com>
2021-03-02 20:06:50 +0900

    Update CodeMemo.cs

--- CodeMemo.cs
+++ CodeMemo.cs
@@ -9,3 +9,11 @@ var bl = DC.MainTable.Data[1].Any( v => v.Value.Contains(word));
 
 //すべての列を対象とし、キーワード(word)が一致すればばtrue
 var bl = DC.MainTable.Data[1].Any( v => v.Value == word);
+
+//すべての列対象で、吉田類と一致する列がある場合
+var ss = DC.MainTable.Search(new List<string> { ""},new List<string> {"吉田類" },new List<string> {"=" },"and");
+var dd = DC.MainTable.Data.Where(x => x.Values.Any(y => y == "吉田類")).ToList();
+
+//すべての列対象で、吉田類を含むする列がある場合
+var ss = DC.MainTable.Search(new List<string> { ""},new List<string> {"吉田類" },new List<string> {"like" },"and");
+var dd = DC.MainTable.Data.Where(x => x.Values.Any(y => y.Contains("吉田類"))).ToList();
