6e2b4af503e28a5b262c702ba3ea857677513c8d
takatlssx <69699607+takatlssx@users.noreply.github.com>
2021-03-02 19:52:43 +0900

    Update CodeMemo.cs

--- CodeMemo.cs
+++ CodeMemo.cs
@@ -1,1 +1,11 @@
+//or検索
+List<T>1.Union(List<T>2);
 
+//and検索
+List<T>1.Intersect(List<T>2)
+
+//すべての列を対象とし、キーワード(word)が含まれていればtrue
+var bl = DC.MainTable.Data[1].Any( v => v.Value.Contains(word));
+
+//すべての列を対象とし、キーワード(word)が一致すればばtrue
+var bl = DC.MainTable.Data[1].Any( v => v.Value == word);
