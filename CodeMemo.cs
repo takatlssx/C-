757f7550e6b6d2f9be42b29bb3381064f3df18bd
takatlssx <69699607+takatlssx@users.noreply.github.com>
2021-03-10 13:14:24 +0900

    Update CodeMemo.cs

--- CodeMemo.cs
+++ CodeMemo.cs
@@ -25,73 +25,11 @@ var dd = DC.MainTable.Data.Where(x => x.Values.Any(y => y.Contains("吉田類"))
 //以上が「true」であれば不正なidが含まれている
 if(idList.Any(x => !Index.Contains[x] && x!="全て" && x!=""))
 {
-      return false;
+    return false;
 }
 
 //フォームコントロールのチェックボックスを全て未チェックにする
 foreach (var c in Cntrols.Cast<Control>().Where(c => c is CheckBox))
 {
     c.Checked = false;
-}
-
-public List<Dictionary<string,string>> Search(List<string> idList,List<string> wordList,List<string> operandList,andOr = "or")
-{
-      //idListチェック
-      if(idList.Any(x => !Index.Contains[x] && x!="全て" && x!=""))
-      {
-            return false;
-      }
-      //idList、wordList、operandListの個数チェック
-      if(idList.Count != wordList.Count || idList.Count != operandList.Count)
-      {
-            return false;
-      }
-      //operandListに"="か"like"以外の文字が指定されていたら自動で"like"にする
-      operandList.Where(x => x!="=" && )
-
-}
-
-public void moveRip(){
-      
-      //ripフォルダのファイル一覧取得
-      var fileList = "";
-      
-      foreach(string fl in fileList)
-      {
-            //ファイル名を_で分割
-            //ファイル名はcategory_tag_series_number_title.拡張子の規則
-            //buuf[0]=category,[1]=tag,[2]=series,[3]=number,[4]=title.拡張子
-            string[] buff = Path.GetFileNameWithoutExtention(fl).Split('-');
-            
-            //移動先のファイルパス形成
-            string newFilePath = "G:\\Movie\\"+category+"\\";
-            if(buff[2] != "")
-            {
-                  newFilePath += buff[2] + "\\"
-            }
-            //上記のフォルダパスが存在しなければ作成
-            if(!Directory.Exists(newFilePath))
-            {
-                  Directory.CreateDirectory(newFilePath);
-            }
-            newFilePath += Path.GetFileName(fl);
-            
-            //ファイルコピー
-            File.Copy(fl,newFilePath);
-            
-            //Indexの個数分の空白の文字の配列を作成
-            string[] vals = Enumerable.Repeat<string>("", Index.Count).ToArray();
-            //Indexと上記空白配列をZip→ToDictionaryで新規データ辞書作成
-            var newData = Index.Zip(vals,(k,v) => new {k,v}).ToDictionary(a => a.k, a => a.v);
-            
-            newData["title"] = buff[4];
-            newData["subtitle"] = buff[4];
-            newData["number"] = int.Parse(buff[3]).ToString("0000");
-            newData["category"] = buff[0];
-            newData["tag"] = buff[1];
-            newData["series"] = buff[2];
-            newData["file"] = newFilePath;
-            
-      }
-      
 }
