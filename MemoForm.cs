69bb808979d418d9e34a07ab62affa9c146edba3
takatlssx <69699607+takatlssx@users.noreply.github.com>
2021-03-12 13:39:09 +0900

    Update MemoForm.cs

--- MemoForm.cs
+++ MemoForm.cs
@@ -1,1 +1,31 @@
+//現在表示するデータ
+public List<Dictionary<string,string>> ViewingData = new List<Dictionary<string,string>>();
 
+//データを検索する条件
+public List<string> SrcIdList = new List<string>();
+
+
+//データグリッドビュー描画
+public void DrawDGV(string sortId = "id",string sortOrder = "asc")
+{
+    dGV.Columuns.Clear();
+    dGV.Rows.Clear();
+    
+    
+}
+
+//dGVのステータスラベル
+public void DrawLabelDGVStatus()
+{
+    if(SrcIdList.Count == 0)
+    {
+        labelDGVStatus.Text = $"全件表示【{ViewingData.Count}件】";
+    }
+    else{
+    }
+}
+
+//リストボックス描画
+public void DrawListBox()
+{
+}
